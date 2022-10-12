cc798f55ea4068163251f98bbc4fad94c87c0b78
Daniel Saelid <saeliddp@attu1.cs.washington.edu>
2022-10-12 00:23:07 -0700

    seq generator for characters

--- constants.py
+++ constants.py
@@ -1,5 +1,8 @@
 from enum import Enum
 
+WORD_START = '^'
+NUMERIC = '#'
+
 class VietDiacriticMark(Enum):
   # NONTONEMARK_TONEMARK
   NONE_NONE = 0
@@ -9,7 +12,7 @@ class VietDiacriticMark(Enum):
   NONE_TILDE = 4
   NONE_DOT = 5
 
-  CROSS_NONE = 6 # only used on d, which can't have accent
+  CROSS_NONE = 6 # only used on d, which can't have tone mark
 
   HAT_NONE = 7
   HAT_GRAVE = 8
@@ -32,7 +35,7 @@ class VietDiacriticMark(Enum):
   SWOOP_TILDE = 23
   SWOOP_DOT = 24
 
-viet_char_to_mark = {}
+viet_char_to_mark = {} # will contain all besides NONE_NONE
 
 for c in ['à', 'è', 'ì', 'ò', 'ù']: viet_char_to_mark[c] = VietDiacriticMark.NONE_GRAVE
 for c in ['á', 'é', 'í', 'ó', 'ú']: viet_char_to_mark[c] = VietDiacriticMark.NONE_ACUTE
@@ -42,8 +45,27 @@ for c in ['ạ', 'ẹ', 'ị', 'ị', 'ụ']: viet_char_to_mark[c] = VietDiacrit
 
 viet_char_to_mark['đ'] = VietDiacriticMark.CROSS_NONE
 
-for c in ['à', 'è', 'ì', 'ò', 'ù']: viet_char_to_mark[c] = VietDiacriticMark.HAT_GRAVE
-for c in ['á', 'é', 'í', 'ó', 'ú']: viet_char_to_mark[c] = VietDiacriticMark.HAT_ACUTE
-for c in ['ả', 'ẻ', 'ỉ', 'ỏ', 'ủ']: viet_char_to_mark[c] = VietDiacriticMark.HAT_HOOK
-for c in ['ã', 'ẽ', 'ĩ', 'õ', 'ũ']: viet_char_to_mark[c] = VietDiacriticMark.HAT_TILDE
-for c in ['ạ', 'ẹ', 'ị', 'ị', 'ụ']: viet_char_to_mark[c] = VietDiacriticMark.HAT_DOT
+for c in ['â', 'ê', 'ô']: viet_char_to_mark[c] = VietDiacriticMark.HAT_NONE
+for c in ['ầ', 'ề', 'ồ']: viet_char_to_mark[c] = VietDiacriticMark.HAT_GRAVE
+for c in ['ấ', 'ế', 'ố']: viet_char_to_mark[c] = VietDiacriticMark.HAT_ACUTE
+for c in ['ẩ', 'ể', 'ổ']: viet_char_to_mark[c] = VietDiacriticMark.HAT_HOOK
+for c in ['ẫ', 'ễ', 'ỗ']: viet_char_to_mark[c] = VietDiacriticMark.HAT_TILDE
+for c in ['ậ', 'ệ', 'ộ']: viet_char_to_mark[c] = VietDiacriticMark.HAT_DOT
+
+for c in ['ư', 'ơ']: viet_char_to_mark[c] = VietDiacriticMark.TAIL_NONE
+for c in ['ừ', 'ờ']: viet_char_to_mark[c] = VietDiacriticMark.TAIL_GRAVE
+for c in ['ứ', 'ớ']: viet_char_to_mark[c] = VietDiacriticMark.TAIL_ACUTE
+for c in ['ử', 'ở']: viet_char_to_mark[c] = VietDiacriticMark.TAIL_HOOK
+for c in ['ữ', 'ỡ']: viet_char_to_mark[c] = VietDiacriticMark.TAIL_TILDE
+for c in ['ự', 'ợ']: viet_char_to_mark[c] = VietDiacriticMark.TAIL_DOT
+
+viet_char_to_mark['ă'] = VietDiacriticMark.SWOOP_NONE
+viet_char_to_mark['ằ'] = VietDiacriticMark.SWOOP_GRAVE
+viet_char_to_mark['ắ'] = VietDiacriticMark.SWOOP_ACUTE
+viet_char_to_mark['ẳ'] = VietDiacriticMark.SWOOP_HOOK
+viet_char_to_mark['ẵ'] = VietDiacriticMark.SWOOP_TILDE
+viet_char_to_mark['ặ'] = VietDiacriticMark.SWOOP_DOT
+
+def get_mark_viet(character: str) -> VietDiacriticMark:
+  c = character.lower()
+  return viet_char_to_mark[c] if c in viet_char_to_mark else VietDiacriticMark.NONE_NONE
--- preproc.py
+++ preproc.py
@@ -1,2 +1,33 @@
-def char_seq_generator():
-  pass
+from constants import *
+import re
+
+def char_seq_generator(src_fpath: str, target_fpath: str):
+  src = open(src_fpath, 'r', encoding='utf-8')
+  target = open(target_fpath, 'r', encoding='utf-8')
+  pattern = r'[\s\W]'
+
+  for src_line in src:
+    target_line = target.readline()
+    src_sylls = re.split(pattern, src_line)
+    target_sylls = re.split(pattern, target_line)
+
+    chars = []
+    tags = []
+    for src_syll, target_syll in zip(src_sylls, target_sylls):
+      if src_syll and target_syll:
+        chars.append(WORD_START)
+        tags.append(VietDiacriticMark.NONE_NONE)
+        if re.match(r'[0-9]', src_syll):
+          chars.append(NUMERIC)
+          tags.append(VietDiacriticMark.NONE_NONE)
+        else:
+          i = 0
+          for src_char in src_syll:
+            chars.append(src_char)
+            tags.append(get_mark_viet(target_syll[i]))
+            i += 1
+
+    yield chars, tags
+
+  src.close()
+  target.close()
